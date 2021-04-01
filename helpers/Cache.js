b53118326f71fd9dbd6f650c3a056a3fb55fc61b
ICEPrey <40725969+ICEPrey@users.noreply.github.com>
2021-03-31 19:06:55 -0700

    use prettier

--- helpers/Cache.js
+++ helpers/Cache.js
@@ -3,131 +3,148 @@ const path = require("path");
 const Helper = require("./Helper.js");
 
 module.exports = class Cache {
-	static files = {};
-	static urls = {};
-	static parsers = {};
-	static fetchInProgress = [];
+  static files = {};
+  static urls = {};
+  static parsers = {};
+  static fetchInProgress = [];
 
-	static GetFileLocal(name, fileName, maxTime = 60 * 60 * 1000, parser = null, extraArg = []) {
-		return new Promise(async (resolve, reject) => {
-			let file = this.files[name];
-			if (file) {
-				let difference = Date.now() - file.time;
-				if (difference <= maxTime) {
-					let content = file.content;
-					if (typeof parser === "function") {
-						try {
-							content = await parser(content, ...extraArg);
-						} catch (err) {
-							reject(err);
-							return;
-						}
-					}
+  static GetFileLocal(
+    name,
+    fileName,
+    maxTime = 60 * 60 * 1000,
+    parser = null,
+    extraArg = []
+  ) {
+    return new Promise(async (resolve, reject) => {
+      let file = this.files[name];
+      if (file) {
+        let difference = Date.now() - file.time;
+        if (difference <= maxTime) {
+          let content = file.content;
+          if (typeof parser === "function") {
+            try {
+              content = await parser(content, ...extraArg);
+            } catch (err) {
+              reject(err);
+              return;
+            }
+          }
 
-					resolve(content);
-					return;
-				}
-			}
+          resolve(content);
+          return;
+        }
+      }
 
-			let filePath = path.join(__dirname, "..", "local", fileName);
-			if (!fs.existsSync(filePath)) {
-				reject(new Error("Failed to find file in path \"" + filePath + "\""));
-				return;
-			}
+      let filePath = path.join(__dirname, "..", "local", fileName);
+      if (!fs.existsSync(filePath)) {
+        reject(new Error('Failed to find file in path "' + filePath + '"'));
+        return;
+      }
 
-			let buffer = fs.readFileSync(filePath);
-			let content = buffer.toString(buffer.readUInt16LE(0) === 65279 ? "ucs2" : "utf8");
+      let buffer = fs.readFileSync(filePath);
+      let content = buffer.toString(
+        buffer.readUInt16LE(0) === 65279 ? "ucs2" : "utf8"
+      );
 
-			this.files[name] = {
-				time: Date.now(),
-				content: content
-			};
-			this.parsers[name] = parser;
-			this.urls[name] = undefined;
+      this.files[name] = {
+        time: Date.now(),
+        content: content
+      };
+      this.parsers[name] = parser;
+      this.urls[name] = undefined;
 
-			if (typeof parser === "function") {
-				try {
-					content = await parser(content);
-				} catch (err) {
-					reject(err);
-					return;
-				}
-			}
+      if (typeof parser === "function") {
+        try {
+          content = await parser(content);
+        } catch (err) {
+          reject(err);
+          return;
+        }
+      }
 
-			resolve(content);
-		});
-	}
+      resolve(content);
+    });
+  }
 
-	static GetFile(url, name, maxTime = 60 * 60 * 1000, parser = null, extraArg = []) {
-		return new Promise(async (resolve, reject) => {
-			if (arguments.length === 1 || arguments.length === 2) {
-				if (arguments.length === 2) {
-					maxTime = name;
-				}
+  static GetFile(
+    url,
+    name,
+    maxTime = 60 * 60 * 1000,
+    parser = null,
+    extraArg = []
+  ) {
+    return new Promise(async (resolve, reject) => {
+      if (arguments.length === 1 || arguments.length === 2) {
+        if (arguments.length === 2) {
+          maxTime = name;
+        }
 
-				name = url;
-				url = this.urls[name];
-				parser = this.parsers[name];
-			}
+        name = url;
+        url = this.urls[name];
+        parser = this.parsers[name];
+      }
 
-			let file = this.files[name];
-			if (file) {
-				let difference = Date.now() - file.time;
-				if (difference <= maxTime) {
-					let content = file.content;
-					if (typeof parser === "function") {
-						try {
-							content = await parser(content, ...extraArg);
-						} catch (err) {
-							reject(err);
-							return;
-						}
-					}
+      let file = this.files[name];
+      if (file) {
+        let difference = Date.now() - file.time;
+        if (difference <= maxTime) {
+          let content = file.content;
+          if (typeof parser === "function") {
+            try {
+              content = await parser(content, ...extraArg);
+            } catch (err) {
+              reject(err);
+              return;
+            }
+          }
 
-					resolve(content);
-					return;
-				}
-			}
+          resolve(content);
+          return;
+        }
+      }
 
-			if (!url) {
-				reject(new Error("No URL available to fetch from"));
-				return;
-			}
+      if (!url) {
+        reject(new Error("No URL available to fetch from"));
+        return;
+      }
 
-			if (this.fetchInProgress.includes(name)) {
-				setTimeout(() => {
-					this.GetFile(url, name, maxTime, parser).then(resolve).catch(reject);
-				}, 2000);
-				return;
-			}
-			this.fetchInProgress.push(name);
+      if (this.fetchInProgress.includes(name)) {
+        setTimeout(() => {
+          this.GetFile(url, name, maxTime, parser).then(resolve).catch(reject);
+        }, 2000);
+        return;
+      }
+      this.fetchInProgress.push(name);
 
-			Helper.GetURL(url).then(async (content) => {
-				this.files[name] = {
-					time: Date.now(),
-					content: content
-				};
-				this.parsers[name] = parser;
-				this.urls[name] = url;
+      Helper.GetURL(url)
+        .then(async (content) => {
+          this.files[name] = {
+            time: Date.now(),
+            content: content
+          };
+          this.parsers[name] = parser;
+          this.urls[name] = url;
 
-				if (typeof parser === "function") {
-					try {
-						content = await parser(content);
-					} catch (err) {
-						reject(err);
-						return;
-					}
-				}
+          if (typeof parser === "function") {
+            try {
+              content = await parser(content);
+            } catch (err) {
+              reject(err);
+              return;
+            }
+          }
 
-				resolve(content);
-			}).catch((err) => {
-				reject(err);
-			}).finally(() => {
-				let index = this.fetchInProgress.indexOf(name);
-				if (index >= 0) {
-					this.fetchInProgress.splice(index, 1);
-				}
-			});
-		});
-	}
+          resolve(content);
+        })
+        .catch((err) => {
+          reject(err);
+        })
+        .finally(() => {
+          let index = this.fetchInProgress.indexOf(name);
+          if (index >= 0) {
+            this.fetchInProgress.splice(index, 1);
+          }
+        });
+    });
+  }
 };
