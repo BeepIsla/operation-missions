d73e00c636b2a7b1adc7f40abb28ff8317b851ba
ICEPrey <40725969+ICEPrey@users.noreply.github.com>
2021-03-31 19:07:29 -0700

    pr request

--- helpers/Helper.js
+++ helpers/Helper.js
@@ -2,62 +2,67 @@ const request = require("request");
 let english = undefined;
 
 module.exports = class Helper {
-	static GetURL(url) {
-		return new Promise((resolve, reject) => {
-			request(url, (err, res, body) => {
-				if (err) {
-					reject(err);
-					return;
-				}
+  static GetURL(url) {
+    return new Promise((resolve, reject) => {
+      request(url, (err, res, body) => {
+        if (err) {
+          reject(err);
+          return;
+        }
 
-				resolve(body);
-			});
-		});
-	}
+        resolve(body);
+      });
+    });
+  }
 
-	static SetEnglishFallback(translation) {
-		let translationTokens = {};
-		for (let key in translation.lang.Tokens) {
-			translationTokens[key.toLowerCase()] = translation.lang.Tokens[key];
-		}
-		english = translationTokens;
-	}
+  static SetEnglishFallback(translation) {
+    let translationTokens = {};
+    for (let key in translation.lang.Tokens) {
+      translationTokens[key.toLowerCase()] = translation.lang.Tokens[key];
+    }
+    english = translationTokens;
+  }
 
-	static GetTranslation(language, token, attribs = {}) {
-		token = token.toString().toLowerCase();
+  static GetTranslation(language, token, attribs = {}) {
+    token = token.toString().toLowerCase();
 
-		if (token.startsWith("#")) {
-			token = token.slice(1);
-		}
+    if (token.startsWith("#")) {
+      token = token.slice(1);
+    }
 
-		let translation = language[token];
-		if (!translation && english) {
-			translation = english[token];
-		}
+    let translation = language[token];
+    if (!translation && english) {
+      translation = english[token];
+    }
 
-		if (!attribs || Object.keys(attribs).length <= 0) {
-			return translation;
-		}
+    if (!attribs || Object.keys(attribs).length <= 0) {
+      return translation;
+    }
 
-		for (let key in attribs) {
-			let regex = new RegExp("{[a-z]:" + key + "}", "i");
-			translation = translation.replace(regex, this.GetTranslation(language, attribs[key]));
-		}
+    for (let key in attribs) {
+      let regex = new RegExp("{[a-z]:" + key + "}", "i");
+      translation = translation.replace(
+        regex,
+        this.GetTranslation(language, attribs[key])
+      );
+    }
 
-		return translation;
-	}
+    return translation;
+  }
 
-	static commandLine = {
-		includes(arg) {
-			return process.argv.join(" ").toLowerCase().includes(arg.toLowerCase());
-		},
-		get(arg) {
-			let index = process.argv.map(a => a.toLowerCase()).indexOf(arg.toLowerCase());
-			if (index <= -1) {
-				return undefined;
-			}
+  static commandLine = {
+    includes(arg) {
+      return process.argv.join(" ").toLowerCase().includes(arg.toLowerCase());
+    },
+    get(arg) {
+      let index = process.argv
+        .map((a) => a.toLowerCase())
+        .indexOf(arg.toLowerCase());
+      if (index <= -1) {
+        return undefined;
+      }
 
-			return process.argv[index + 1];
-		}
-	}
-}
+      return process.argv[index + 1];
+    }
+  };
+};
